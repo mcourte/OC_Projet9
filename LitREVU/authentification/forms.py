8c153544ce1f1807a1b315fdd0d613cee5d24027
Magali Courté <courte.magali@gmail.com>
2024-04-02 10:55:51 +0200

    test migration

--- LitREVU/authentification/forms.py
+++ LitREVU/authentification/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
 from .models import User
 
 
@@ -46,11 +45,3 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-
-
-class UploadProfilePhotoForm(forms.ModelForm):
-
-    class Meta:
-
-        model = get_user_model()
-        fields = ("profile_photo")
