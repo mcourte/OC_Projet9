f8ff8d58042972e62f4114af8ef2d80858636a5e
Magali Courté <courte.magali@gmail.com>
2024-05-15 10:30:03 +0200

    Màj suivant/précédent

--- LitREVU/static/script-js.js
+++ LitREVU/static/script-js.js
@@ -2,7 +2,7 @@
 function addInputHoverEffect() {
     // Obtenir tous les champs d'entrée
     var inputs = document.querySelectorAll('input');
-  
+
     // Parcourir chaque champ d'entrée
     inputs.forEach(function(input) {
         // Ajouter des écouteurs d'événements pour la souris entrante et sortante
@@ -15,18 +15,38 @@ function addInputHoverEffect() {
             this.classList.remove('input-hover');
         });
     });
-  }
-  
-  // JavaScript pour la sélection de la note en cliquant sur les étoiles
-  document.addEventListener("DOMContentLoaded", function() {
+}
+
+// Fonction pour récupérer la valeur du cookie CSRF
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+// Vérifie si l'utilisateur utilise un lecteur d'écran
+function isScreenReaderActive() {
+    return window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
     const stars = document.querySelectorAll(".star");
     const ratingInput = document.getElementById('rating');
-  
+
     stars.forEach(function(star) {
         star.addEventListener("click", function() {
             const rating = parseInt(star.getAttribute("data-rating"));
             ratingInput.value = rating;
-  
+
             // Mettre à jour la couleur des étoiles en fonction de la note sélectionnée
             stars.forEach(function(s) {
                 const sRating = parseInt(s.getAttribute("data-rating"));
@@ -34,10 +54,10 @@ function addInputHoverEffect() {
             });
         });
     });
-  
+
     // Appeler la fonction pour ajouter l'effet au survol des champs d'entrée
     addInputHoverEffect();
-  
+
     // Fonction pour gérer le clic sur le bouton de suppression de ticket
     document.querySelectorAll('.delete-ticket-button').forEach(button => {
         button.addEventListener('click', function(event) {
@@ -49,7 +69,7 @@ function addInputHoverEffect() {
             }
         });
     });
-  
+
     // Fonction pour gérer le clic sur le bouton de suppression de critique
     document.querySelectorAll('.delete-review-button').forEach(button => {
         button.addEventListener('click', function(event) {
@@ -80,26 +100,10 @@ function addInputHoverEffect() {
             }
         });
     });
-  
-    // Fonction pour récupérer la valeur du cookie CSRF
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-  
+
     // Fonction pour gérer la sélection de la note en cliquant sur les étoiles dans le formulaire de critique
     const ratingStars = document.querySelectorAll(".rating .star");
-  
+
     ratingStars.forEach(function(star) {
         star.addEventListener("click", function() {
             const rating = this.getAttribute("data-rating");
@@ -117,49 +121,40 @@ function addInputHoverEffect() {
             });
         });
     });
-  
+
     // Fonction pour afficher une page de tickets et de critiques
     const tickets = document.querySelectorAll('.ticket');
     const reviews = document.querySelectorAll('.review');
-    const itemsPerPage = 2;
+    const itemsPerPage = 5;
     let currentPage = 1;
-  
+
     function showPage(page) {
         const startIndex = (page - 1) * itemsPerPage;
         const endIndex = startIndex + itemsPerPage;
-  
+
         tickets.forEach((ticket, index) => {
             ticket.style.display = (index >= startIndex && index < endIndex) ? 'block' : 'none';
         });
-  
+
         reviews.forEach((review, index) => {
             review.style.display = (index >= startIndex && index < endIndex) ? 'block' : 'none';
         });
-  
+
         const totalPages = Math.ceil(Math.max(tickets.length, reviews.length) / itemsPerPage);
         const prevButton = document.getElementById('prevPage');
         const nextButton = document.getElementById('nextPage');
-  
-        if (currentPage === 1) {
-            prevButton.style.display = 'none';
-        } else {
-            prevButton.style.display = 'block';
-        }
-  
-        if (currentPage === totalPages) {
-            nextButton.style.display = 'none';
-        } else {
-            nextButton.style.display = 'block';
-        }
+
+        prevButton.style.display = (currentPage === 1) ? 'none' : 'block';
+        nextButton.style.display = (currentPage === totalPages) ? 'none' : 'block';
     }
-  
+
     document.getElementById('prevPage').addEventListener('click', function() {
         if (currentPage > 1) {
             currentPage--;
             showPage(currentPage);
         }
     });
-  
+
     document.getElementById('nextPage').addEventListener('click', function() {
         const totalPages = Math.ceil(Math.max(tickets.length, reviews.length) / itemsPerPage);
         if (currentPage < totalPages) {
@@ -167,16 +162,11 @@ function addInputHoverEffect() {
             showPage(currentPage);
         }
     });
-  
+
     showPage(currentPage);
-  });
 
-// Verifie si l'utilisateur utilise un lecteur d'écran
-function isScreenReaderActive() {
-    return window.matchMedia('(prefers-reduced-motion: reduce)').matches;
-}
-
-if (isScreenReaderActive()) {
-    // Ajoutez une classe CSS pour afficher le champ de description de l'image
-    document.querySelector('.image-description').classList.remove('sr-only');
-}
+    if (isScreenReaderActive()) {
+        // Ajoutez une classe CSS pour afficher le champ de description de l'image
+        document.querySelector('.image-description').classList.remove('sr-only');
+    }
+});
